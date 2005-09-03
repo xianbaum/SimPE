b9a8a3ad308dcaf20fb13ad816028457376f4a3c
Peter L Jones <peter@drealm.info>
2005-09-03 12:48:24 +0000

    Str: Oops - Add String Appending to Release

--- simpe-lite/SimPe BHAV/StrForm.cs
+++ simpe-lite/SimPe BHAV/StrForm.cs
@@ -85,10 +85,6 @@ namespace SimPe.PackedFiles.UserInterface
 
 			Control[] ab = { btnBigString, btnBigDesc };
 			alBigBtn = new ArrayList(ab);
-
-#if !(INPROGRESS || DEBUG)
-			this.btnAppend.Visible = false;
-#endif
 		}
 
 		/// <summary>
