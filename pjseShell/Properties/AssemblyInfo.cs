d14695a7ccc4287d273b31845f786f225cc3c626
Peter L Jones <peter@drealm.info>
2006-11-15 20:12:30 +0000

    Pretend this isn't even here

---
+++ pjseShell/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pjseShell")]
+[assembly: AssemblyDescription("A slim outer shell to edit Sims2 packages using the PJSE plugins.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter L Jones")]
+[assembly: AssemblyProduct("pjseShell")]
+[assembly: AssemblyCopyright("Copyright © The Dwarfen Realm 2006.  Released under the GNU GPL.  See LICENSE.txt.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("34d609f3-ca59-427a-a6c0-f4f4e73dbd2c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
