085c442a53e8feb8aa18b1690b1d6c0f607493f5
Peter L Jones <peter@drealm.info>
2008-03-22 11:13:03 +0000

    Wiz0x0020 Test Object Type created

--- simpe-lite/SimPe BHAV/BhavWizPrim.cs
+++ simpe-lite/SimPe BHAV/BhavWizPrim.cs
@@ -1797,7 +1797,12 @@ namespace pjse.BhavNameWizards
 	{
 		public WizPrim0x0020(Instruction i) : base(i) { }
 
-		protected override string Operands(bool lng)
+        public override ABhavOperandWiz Wizard()
+        {
+            return new pjse.BhavOperandWizards.BhavOperandWiz0x0020(instruction);
+        }
+
+        protected override string Operands(bool lng)
 		{
 			byte[] o = new byte[16];
 			((byte[])instruction.Operands).CopyTo(o, 0);
