54ea39e43ab85652b6753c22ba69f875d4f757b9
Peter L Jones <peter@drealm.info>
2008-11-08 10:02:37 +0000

    Fix "View" BHAV (make it fill form); change FileTable refresh messages again

--- simpe-lite/pjse filetable/FileTable.cs
+++ simpe-lite/pjse filetable/FileTable.cs
@@ -107,13 +107,18 @@ namespace pjse
             pfByTypeGroupInstance = new Hashtable();
 
             if (loadEverything)
+            {
+                if (SimPe.Wait.Running) { SimPe.Wait.Progress = 0; SimPe.Wait.MaxProgress = SimPe.FileTable.DefaultFolders.Count; }
                 foreach (SimPe.FileTableItem fii in SimPe.FileTable.DefaultFolders)
-                    if (fii.Use) Add(fii.Name, fii.IsRecursive, fii.Type.AsExpansions != SimPe.Expansions.Custom, true);
+                    if (fii.Use)
+                        Add(fii.Name, fii.IsRecursive, fii.Type.AsExpansions, true);
+                if (SimPe.Wait.Running) SimPe.Wait.MaxProgress = 0;
+            }
 
-            this.Add(Path.Combine(SimPe.Helper.SimPePluginPath, "pjse.coder.plugin\\GlobalStrings.package"), false, false, true);
+            this.Add(Path.Combine(SimPe.Helper.SimPePluginPath, "pjse.coder.plugin\\GlobalStrings.package"), false, SimPe.Expansions.Custom, true);
 
             if (loadEverything)
-                this.Add(Path.Combine(SimPe.Helper.SimPePluginDataPath, "pjse.coder.plugin\\Includes"), true, false, true);
+                this.Add(Path.Combine(SimPe.Helper.SimPePluginDataPath, "pjse.coder.plugin\\Includes"), true, SimPe.Expansions.Custom, true);
 
             string packages_txt = Path.Combine(SimPe.Helper.SimPePluginDataPath, "pjse.coder.plugin\\packages.txt");
             if (loadEverything)
@@ -121,7 +126,7 @@ namespace pjse
                 {
                     System.IO.StreamReader sr = new StreamReader(packages_txt);
                     for (string line = sr.ReadLine(); line != null; line = sr.ReadLine())
-                        this.Add(line.TrimEnd('+'), line.EndsWith("+"), false, true);
+                        this.Add(line.TrimEnd('+'), line.EndsWith("+"), SimPe.Expansions.Custom, true);
                     sr.Close();
                     sr.Dispose();
                     sr = null;
@@ -195,22 +200,22 @@ namespace pjse
         }
 
 
-        private void Add(string v, bool recurse, bool isMaxis, bool isFixed)
+        private void Add(string v, bool recurse, SimPe.Expansions ep, bool isFixed)
         {
-            wm("Loading " + System.IO.Path.GetFileName(v).Replace(".package", ""));
+            wm("Loading " + ep + " " + System.IO.Path.GetFileName(v).Replace(".package", ""));
             System.Windows.Forms.Application.DoEvents();
             if (Directory.Exists(v))
             {
                 foreach (string i in Directory.GetFiles(v, "*.package"))
-                    Add(i, false, isMaxis, isFixed);
+                    Add(i, false, ep, isFixed);
 
                 if (recurse)
                     foreach (string i in Directory.GetDirectories(v))
-                        Add(i, true, isMaxis, isFixed);
+                        Add(i, true, ep, isFixed);
             }
 
             else if (!v.ToLowerInvariant().EndsWith(SimPe.Helper.PATH_SEP+"globalcatbin.bundle.package") && File.Exists(v))
-                Add(SimPe.Packages.File.LoadFromFile(v), isMaxis, isFixed);
+                Add(SimPe.Packages.File.LoadFromFile(v), ep != SimPe.Expansions.Custom, isFixed);
         }
 
         
@@ -219,12 +224,8 @@ namespace pjse
 			if (package == null) return;
 			if (pfByPackage[package] != null) return;
 
-            if (SimPe.Wait.Running) { SimPe.Wait.Progress = 0; SimPe.Wait.MaxProgress = package.Index.Length; }
             foreach (IPackedFileDescriptor i in package.Index)
-            {
                 Add(new Entry(package, i, isMaxis, isFixed));
-                if (SimPe.Wait.Running) SimPe.Wait.Progress++;
-            }
 
             if (isMaxis)
                 maxisPackages.Add(package);
